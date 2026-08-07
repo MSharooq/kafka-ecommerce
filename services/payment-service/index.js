9d4e67d6a4e1d8bf84d380abb0979c2d548d550e
MSharooq <sharooqlavanya04@gmail.com>
2026-08-07 23:48:12 +0530

    added payment service

--- services/payment-service/index.js
+++ services/payment-service/index.js
@@ -1,17 +1,28 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-    origin:"https://localhost:3000",
-}))
+app.use(cors());
 
-app.use(express.json)
+app.use(express.json());
+
+app.post("/payment-service", async (req, res) => {
+  const { cart } = req.body;
+  const userId = "123";
+
+  //TODO: payemnt
+
+  //add kafka here
+  console.log("Payment endpoint hit");
+  return res.status(200).send("payment Success");
+});
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).send(err.message)
-})
+  console.log(err);
+  res.status(err.status || 500).send(err.message);
+});
 
-app.listen(8000,  () => {
-    console.log("Payment service lsitening on port 8000")
-})
+app.listen(8000, () => {
+  console.log("Payment service lsitening on port 8000");
+});
